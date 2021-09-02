4dbe9345dfef785a2de230f70eabd69343b2e912
cosinehere <975745601@qq.com>
2021-09-02 16:19:21 +0800

    update test_uint128.cpp

--- test_uint128.cpp
+++ test_uint128.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 
-    uint128_t a(10);
+    uint128_t a(0, 10);
     uint128_t b(a);
 
     printf("a == b %d\n", a == b);
@@ -14,7 +14,15 @@ int main() {
     printf("a < b %d\n", a < b);
     printf("a <= b %d\n", a <= b);
 
-    uint128_t c = a & (b + 1);
-    printf("c.l %lu c.h %lu a && b %d\n", c.high(), c.low(), a && b);
+    uint128_t c = a & (b + uint128_t(0, 1));
+    b += uint128_t(0, 1);
+    printf("c.l %lu c.h %lu\na == b %d\n", c.high(), c.low(), a == b);
+
+    uint128_t d = uint128_t(0x10000ul, 0x123) * uint128_t(2, 1);
+    printf("d.h %016lx, d.l %016lx\n", d.high(), d.low());
+
+    uint128_t e = uint128_t(0x21, 0x0) / uint128_t(0x0, 0x2);
+    printf("e.h %016lx, e.l %016lx\n", e.high(), e.low());
+
     return 0;
 }
